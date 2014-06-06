0267b463e6dc370de83af2582046646a45f9f92b
andycnzh <andycnzh@outlook.com>
2014-06-06 14:23:56 +0800

    Init

---
+++ ConsoleApplicationTest/ConsoleApplicationTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------");
+
+            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss"));
+            Console.WriteLine(Guid.NewGuid().ToString ().Replace('-',' '));
+
+            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff") + (new Random(1000)).Next());
+            Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
+            Console.WriteLine("------");
+            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss"));
+            Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
+            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff") + (new Random(1000)).Next());
+            Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
+            Console.WriteLine("------");
+            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff") + (new Random(1000)).Next());
+            Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
+            Console.WriteLine("------");
+            Console.WriteLine(DateTime.Now.Ticks.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
