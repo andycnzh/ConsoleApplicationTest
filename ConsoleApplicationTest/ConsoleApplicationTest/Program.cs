f467a8d4c5c00b50265a5908e2ecefc921aa0e8e
andycnzh <andycnzh@outlook.com>
2014-06-26 16:05:38 +0800

    Add double max test code

--- ConsoleApplicationTest/ConsoleApplicationTest/Program.cs
+++ ConsoleApplicationTest/ConsoleApplicationTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleApplicationTest
@@ -30,36 +31,86 @@ namespace ConsoleApplicationTest
 
             //Console.ReadLine();
 
-            TestVariable tv = new TestVariable();
+            //TestVariable tv = new TestVariable();
 
-            int a = 1;
-            int b = 2;
-            Console.WriteLine(a);
-            tv.Add(a, b);
-            Console.WriteLine(a);
+            //int a = 1;
+            //int b = 2;
+            //Console.WriteLine(a);
+            //tv.Add(a, b);
+            //Console.WriteLine(a);
 
-            string str = "abcdefg";
-            Console.WriteLine(str);
-            tv.AddStr(str);
-            Console.WriteLine(str);
+            //string str = "abcdefg";
+            //Console.WriteLine(str);
+            //tv.AddStr(str);
+            //Console.WriteLine(str);
 
-            StringBuilder sb = new StringBuilder("123456");
-            Console.WriteLine(sb.ToString());
-            tv.UpdateStringBuilder(sb);
-            Console.WriteLine(sb.ToString ());
+            //StringBuilder sb = new StringBuilder("123456");
+            //Console.WriteLine(sb.ToString());
+            //tv.UpdateStringBuilder(sb);
+            //Console.WriteLine(sb.ToString ());
 
-            TestA myA = new TestA();
-            myA.i = 3;
-            myA.Name = "C";
-            Console.WriteLine(myA.ToString());
-            tv.UpdateClassA(myA);
-            Console.WriteLine(myA.ToString());
+            //TestA myA = new TestA();
+            //myA.i = 3;
+            //myA.Name = "C";
+            //Console.WriteLine(myA.ToString());
+            //tv.UpdateClassA(myA);
+            //Console.WriteLine(myA.ToString());
 
-            Console.WriteLine(tv.UpdateClassB(myA));
-            Console.WriteLine(myA.ToString());
+            //Console.WriteLine(tv.UpdateClassB(myA));
+            //Console.WriteLine(myA.ToString());
 
-            Console.WriteLine(tv.UpdateClassC(ref myA));
-            Console.WriteLine(myA.ToString());
+            //Console.WriteLine(tv.UpdateClassC(ref myA));
+            //Console.WriteLine(myA.ToString());
+
+            string doubleMax = double.MaxValue.ToString();
+            Console.WriteLine(doubleMax);
+
+            Regex regex = new Regex(@"(\d*.\d*)E([+ -]?\d*)");
+            string p = regex.Replace(doubleMax, "$1");
+            Console.WriteLine(p);
+
+            p = Convert.ToString((double.Parse(p) + 0.1));
+            Console.WriteLine(p);
+
+            Console.WriteLine(regex.Replace(doubleMax, "$2"));
+
+            doubleMax = p + "E" + regex.Replace(doubleMax, "$2");
+
+            try
+            {
+                Console.WriteLine(double.Parse(doubleMax));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("---");
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine("---");
+
+            try
+            {
+                Console.WriteLine(double.Parse(decimal.MaxValue.ToString()));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("===");
+
+            string doubleMax1 = (double.MaxValue + long.MaxValue).ToString();
+            Console.WriteLine(doubleMax1);
+
+
+
+            string doubleMin = double.MinValue.ToString();
+            Console.WriteLine(doubleMin);
+
+            string longMax = long.MaxValue.ToString();
+            Console.WriteLine(longMax);
 
             Console.ReadLine();
         }
