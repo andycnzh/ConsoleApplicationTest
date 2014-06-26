354eb88bb54c2d0e9cd0effed02ced2769821b19
andycnzh <andycnzh@outlook.com>
2014-06-26 13:51:09 +0800

    Add variable test

--- ConsoleApplicationTest/ConsoleApplicationTest/Program.cs
+++ ConsoleApplicationTest/ConsoleApplicationTest/Program.cs
@@ -10,23 +10,50 @@ namespace ConsoleApplicationTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("------");
+            //Console.WriteLine("------");
 
-            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss"));
-            Console.WriteLine(Guid.NewGuid().ToString ().Replace('-',' '));
+            //Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss"));
+            //Console.WriteLine(Guid.NewGuid().ToString ().Replace('-',' '));
 
-            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff") + (new Random(1000)).Next());
-            Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
-            Console.WriteLine("------");
-            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss"));
-            Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
-            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff") + (new Random(1000)).Next());
-            Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
-            Console.WriteLine("------");
-            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff") + (new Random(1000)).Next());
-            Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
-            Console.WriteLine("------");
-            Console.WriteLine(DateTime.Now.Ticks.ToString());
+            //Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff") + (new Random(1000)).Next());
+            //Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
+            //Console.WriteLine("------");
+            //Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss"));
+            //Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
+            //Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff") + (new Random(1000)).Next());
+            //Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
+            //Console.WriteLine("------");
+            //Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssfff") + (new Random(1000)).Next());
+            //Console.WriteLine(Guid.NewGuid().ToString().Replace('-', ' '));
+            //Console.WriteLine("------");
+            //Console.WriteLine(DateTime.Now.Ticks.ToString());
+
+            //Console.ReadLine();
+
+            TestVariable tv = new TestVariable();
+
+            int a = 1;
+            int b = 2;
+            Console.WriteLine(a);
+            tv.Add(a, b);
+            Console.WriteLine(a);
+
+            string str = "abcdefg";
+            Console.WriteLine(str);
+            tv.AddStr(str);
+            Console.WriteLine(str);
+
+            StringBuilder sb = new StringBuilder("123456");
+            Console.WriteLine(sb.ToString());
+            tv.UpdateStringBuilder(sb);
+            Console.WriteLine(sb.ToString ());
+
+            TestA myA = new TestA();
+            myA.i = 3;
+            myA.Name = "C";
+            Console.WriteLine(myA.ToString());
+            tv.UpdateClassA(myA);
+            Console.WriteLine(myA.ToString());
 
             Console.ReadLine();
         }
---
+++ ConsoleApplicationTest/ConsoleApplicationTest/TestVariable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationTest
+{
+    class TestVariable
+    {
+        public int Add(int a,int b)
+        {
+            a = a + b;
+            return a;
+        }
+
+        public string AddStr(string str)
+        {
+            str = str + DateTime.Now.ToString();
+
+            return str;
+        }
+
+        public string UpdateStringBuilder(StringBuilder sb)
+        {
+            sb.Append(DateTime.Now.ToString());
+
+            return sb.ToString ();
+        }
+
+        public string UpdateClassA(TestA a)
+        {
+            a.i = 2;
+            a.Name = "B";
+
+            return a.ToString();
+        }
+    }
+
+    class TestA
+    {
+        public int i;
+        public string Name;
+
+        public TestA()
+        {
+            i = 1;
+            Name = "A";
+        }
+
+        public string ToString()
+        {
+            return i.ToString() + Name;
+        }
+    }
+}
